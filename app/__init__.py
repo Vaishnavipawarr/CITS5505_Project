df1d4fc9418a5c1da252ea1cdcf2152bc3f5cfe8
Lawlee-L <lorena.l.luo@outlook.com>
2026-05-06 21:11:29 +0800

    Update Flask routes and frontend structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="../frontend",
+    static_folder="../frontend",
+    static_url_path=""
+)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,37 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
-@app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@app.route('/cusProfile')
-def profile():
-    return render_template('customer-profile.html')
 
-@app.route('/restaurantOverview')
-def restaurantOverview():
-    return render_template('restaurant-overview.html')
+@app.route('/customer-dashboard')
+def customer_dashboard():
+    return render_template('customer-dashboard.html')
 
-@app.route('/restaurantProfile')
-def restaurantProfile():
-    return render_template('restaurant-profile.html')
 
-@app.route('/review')
-def review():
-    return render_template('review.html')
+@app.route('/owner-dashboard')
+def owner_dashboard():
+    return render_template('owner-dashboard.html')
+
+
+@app.route('/restaurants')
+def restaurants():
+    return render_template('restaurants.html')
+
+
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
